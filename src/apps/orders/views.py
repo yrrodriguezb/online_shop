d37736dae9ec2a5634f3cf96f1642d9cc4063788
Yeison Rodriguez <yrrodriguezb@gmail.com>
2021-04-08 22:15:11 -0500

    Generando PDF - Factura del pedido

--- src/apps/orders/views.py
+++ src/apps/orders/views.py
@@ -1,6 +1,10 @@
+from django.conf import settings
 from django.contrib.admin.views.decorators import staff_member_required
+from django.http import HttpResponse
 from django.urls import reverse
 from django.shortcuts import render, redirect, get_object_or_404
+from django.template.loader import render_to_string
+import weasyprint
 from apps.cart.cart import Cart
 from .models import Order, OrderItem
 from .forms import OrderCreateForm
@@ -37,3 +41,19 @@ def order_create(request):
 def admin_order_detail(request, order_id):
     order = get_object_or_404(Order, id=order_id)
     return render(request, 'admin/orders/order/detail.html',{ 'order': order })
+
+
+@staff_member_required
+def admin_order_pdf(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    html = render_to_string('orders/order/pdf.html', { 'order': order })
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = f'filename=order_{order.id}.pdf'
+    
+    weasyprint \
+        .HTML(string=html) \
+        .write_pdf(
+            response,
+            stylesheets=[weasyprint.CSS(str(settings.STATIC_ROOT) + '/css/pdf.css')]
+        )
+    return response
--- src/shop/settings.py
+++ src/shop/settings.py
@@ -129,6 +129,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = BASE_DIR / 'static/'
+
 STATICFILES_DIRS = [
     BASE_DIR / 'apps' / 'static',
 ]
