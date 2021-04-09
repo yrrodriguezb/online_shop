6f637125b0facf5ac19be46e813ecb50276fa2d9
Yeison Rodriguez <yrrodriguezb@gmail.com>
2021-04-08 22:15:11 -0500

    Generando PDF - Factura del pedido

--- src/apps/orders/admin.py
+++ src/apps/orders/admin.py
@@ -39,6 +39,13 @@ def order_detail(obj):
     return mark_safe(f'<a href="{url}">View</a>')
 
 
+def order_pdf(obj):
+    url = reverse('orders:admin_order_pdf', args=[obj.id])
+    return mark_safe(f'<a href="{url}" target="_blank">PDF</a>')
+
+order_pdf.short_description = 'Invoice'
+
+
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ['product']
@@ -49,7 +56,7 @@ class OrderAdmin(admin.ModelAdmin):
     list_display = [
       'id', 'first_name', 'last_name', 'email',
       'address', 'postal_code', 'city', 'paid',
-      'created', 'updated', order_detail
+      'created', 'updated', order_detail, order_pdf
     ]
     #list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
